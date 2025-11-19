b80df403c50d2fca1c4ec3ff2ca8e49039070c12
syedrab <syed.rab@gmail.com>
2025-11-19 14:52:59 -0500

    Add error logging to debug query issues

--- api/video-preview.js
+++ api/video-preview.js
@@ -36,6 +36,8 @@ export default async function handler(req, res) {
       .single();
 
     if (error || !video) {
+      console.error('Supabase query error:', error);
+      console.log('Query params:', { code });
       return res.status(404).send(renderNotFound());
     }
 
