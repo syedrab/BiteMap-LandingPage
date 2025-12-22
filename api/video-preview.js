94b5bba6d296e0c0fe1e27f932a7e39ca776b652
syedrab <syed.rab@gmail.com>
2025-12-22 13:52:32 -0500

    Handle duplicate bunny_video_id

--- api/video-preview.js
+++ api/video-preview.js
@@ -37,7 +37,8 @@ export default async function handler(req, res) {
         .from('Videos')
         .select('*')
         .eq('bunny_video_id', code)
-        .single();
+        .limit(1)
+        .maybeSingle();
 
       video = videoByBunny;
       error = bunnyError;
